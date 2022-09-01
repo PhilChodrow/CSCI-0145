e2425f1b8de45f6f2da162f25fb15d6830c164de
Phil Chodrow <pchodrow@gmail.com>
2022-09-01 19:07:39 -0400

    various updates, syllabus, GD1, HW1

--- assignments/gd-strings.py
+++ assignments/gd-strings.py
@@ -1,7 +1,21 @@
+# --------------------------------
+# GUIDED DISCOVERY
+# STRING INDEXING AND MANIPULATION
+# --------------------------------
+# 
+# In this guided discovery exercise, you'll work with a partner to explore how Python treats string variables. 
+
+# In our first set of exercises, we'll use the following quote by the intersectional antiracist and feminist scholar bell hooks. 
+
+quote = "I will not have my life narrowed down. I will not bow down to somebody else's whim or to someone else's ignorance."
+
+# ----------------
+# PART I: INDEXING
+# ----------------
+
+# Python allows us to 
 
 
 
-def print_4():
-    print(4)
-    
-print_4()
+
+
