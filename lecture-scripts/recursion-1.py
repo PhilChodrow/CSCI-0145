0c41a1e18e369a34c0a3b6eaf012a5d385cf7bb1
Phil Chodrow <pchodrow@gmail.com>
2022-09-20 09:27:33 -0400

    recursion lecture script

--- lecture-scripts/recursion-1.py
+++ lecture-scripts/recursion-1.py
@@ -15,19 +15,70 @@ def length(s):
     """
     compute the length of an input string
     """
-    # base case: this one is easy
+    # base case: if the string is empty (contains no characters)
+    #    return 0
     if s == "":
         return 0
-    # recursive step: now we're closer to the base case
+    # recursive step: if string is not empty:
+    #    return 1 + length of string AFTER 0th character
     else:
         return length(s[1:]) + 1
+
+print("\nLength of strings")
+print("-----------------")
 
 s = "Midd"
 print(length(s))
 print(length(""))
 
+
 """
-PART 1: Math
+PART 1: String Operations
+"""
+
+def count(char, s):
+    """
+    compute the number of times that a character appears in a string
+    """
+    # if string is empty (contains no characters)
+    # return 0
+    if s == "":
+        return 0
+    # if the current (0th) character is equal to char, we found one! 
+    # return the count for the *rest* of the string, plus one
+    elif s[0] == char:
+        return count(char, s[1:]) + 1
+    # if the current (0th) character is not equal to char, then we 
+    # return the count for the *rest* of the string, NOT adding one
+    else:
+        return count(char, s[1:])
+
+print("\nCounting characters in strings")
+print("------------------------------")
+
+s = "to boldly go"
+print(count("o", s))
+print(count("z", s))
+
+def is_palindrome(s):
+    """
+    return True if input string s is a palindrome
+    a palindrome is a string that is the same backwards as it is forwards
+    """
+    if s == "":
+        return True
+    elif s[0] == s[-1]:
+        return is_palindrome(s[1:-1])
+    else:
+        return False
+    
+print("\nPalindrome detector")
+print("-------------------")      
+print(is_palindrome("rotator"))
+print(is_palindrome("potato"))
+
+"""
+PART 2: Math
 """
 
 def sum_up_to(n):
@@ -38,6 +89,9 @@ def sum_up_to(n):
         return 0
     else:
         return sum_up_to(n-1) + n
+
+print("\nSum up to")
+print("---------")      
 
 print(sum_up_to(5))
 print(sum_up_to(100)) # Carl Friedrich Gauss, the famous mathematician,
@@ -53,11 +107,13 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
+print("\nFactorial")
+print("---------")      
 # factorials get very big very quickly 
 print(factorial(5))
 print(factorial(10))
 
-# ACTIVITY: Try to write this on your own! 
+
 def power(x, p):
     """
     compute x raised to the pth power, where p is a positive integer
@@ -66,40 +122,9 @@ def power(x, p):
         return 1
     else:
         return x*power(x, p - 1)
-    
+
+print("\nPower")
+print("-----")      
 print(power(3, 2))
 print(power(5, 3))
     
-
-"""
-PART 2: String Operations
-"""
-
-def count(char, s):
-    """
-    compute the number of times that a character appears in a string
-    """
-    if s == "":
-        return 0
-    elif s[0] == char:
-        return count(char, s[1:]) + 1
-    else:
-        return count(char, s[1:])
-
-print(count("o", s))
-print(count("z", s))
-    
-def is_palindrome(s):
-    """
-    return True if input string s is a palindrome
-    a palindrome is a string that is the same backwards as it is forwards
-    """
-    if s == "":
-        return True
-    elif s[0] == s[-1]:
-        return is_palindrome(s[1:-1])
-    else:
-        return False
-          
-print(is_palindrome("rotator"))
-print(is_palindrome("potato"))
