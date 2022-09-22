f88d0e7d4bf03ad9915d57812d9995671d109e80
Phil Chodrow <pchodrow@gmail.com>
2022-09-22 09:19:03 -0400

    revised GD2

---
+++ assignments/guided-discovery/functions-and-flow/gd2.py
@@ -0,0 +1,10 @@
+# write your function here!
+
+def username_generator(name):
+  pass
+
+# this code below runs your function! Please don't modify.
+if __name__ == "__main__":
+    name = input("Please enter your full name.\n")
+    username = username_generator(name)
+    print("Thank you " + str(name) + "! Your username is " + str(username) + ".")
