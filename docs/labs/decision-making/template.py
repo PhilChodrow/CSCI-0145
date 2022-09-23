d94c0daf475ef9e9a16674d546a9847a56757ea3
Phil Chodrow <pchodrow@gmail.com>
2022-09-22 20:03:21 -0400

    add template file hosted on site

---
+++ docs/labs/decision-making/template.py
@@ -0,0 +1,60 @@
+"""
+GROUP MEMBERS
+
+- partner 1
+- partner 2
+"""
+
+"""
+ACTIVITY 1:
+
+[your bulleted list here]
+"""
+
+"""
+ACTIVITY 2:
+
+[your description of your algorithm here]
+"""
+
+"""
+ACTIVITY 3:
+
+[names of 4 people (your group and another group), and their scores under two algorithms]
+
+[e.g. - name 1: receives a 5 on our algorithm, receives a 7 on other group's algorithm]
+
+
+[brief reflection on whether the rankings are fair]
+"""
+
+"""
+ACTIVITY 4:
+
+[hypothetical test cases and their scores]
+"""
+
+"""
+ACTIVITY 5:
+"""
+
+
+# [replace this comment with your code]
+
+"""
+ACTIVITY 6:
+
+[describe whether your program gave the correct results for your tests cases from activity 4. Go back and make fixes if needed.]
+"""
+
+"""
+ACTIVITY 7:
+
+[Your notes on social assumptions]
+"""
+
+"""
+ACTIVITY 8:
+
+[your reflection on the fairness of your algorithm, your suggestions for improvements, and your description of the choices that lead us to the need for this kind of prioritization]
+"""
