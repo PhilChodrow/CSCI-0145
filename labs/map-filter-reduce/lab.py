39c8819cbfcb92c80fdf8f71bd6f032fb41536ce
Phil Chodrow <pchodrow@gmail.com>
2022-10-06 12:09:32 -0400

    fix bug in well_formed(s)

--- labs/map-filter-reduce/lab.py
+++ labs/map-filter-reduce/lab.py
@@ -14,29 +14,25 @@ ACTIVITY 0
 ACTIVITY 1
 ----------
 """
-
 def well_formed(s):
-    """
-    a function for checking whether an input string s is a well-formed ISBN 
-    according to the criteria outlined in the lab activity.
-    """
     
     # check if length is 10
     digits_10 = len(s) == 10
     
     # check if all chars up to position 8 are digits
     first_9_digits = True
-    for char in s[:9]:
+    for char in s[:-1]:
         if char not in "0123456789":
             first_9_digits = False
             
     # check if final char is a digit or X
     last_digit_ok = True
-    if s[9] not in "0123456789X":
+    if s[-1] not in "0123456789X":
         last_digit_ok = False
     
     # return True if all checks pass
     return digits_10 and first_9_digits and last_digit_ok
+
 
 """
 ----------
