4f0edf54b3886f9e1070fff19983f6d1d827357c
Phil Chodrow <pchodrow@gmail.com>
2022-10-06 09:17:44 -0400

    Update lab.py

--- labs/map-filter-reduce/lab.py
+++ labs/map-filter-reduce/lab.py
@@ -15,6 +15,24 @@ ACTIVITY 1
 ----------
 """
 
+def well_formed(s):
+    
+    # check if length is 10
+    digits_10 = len(s) == 10
+    
+    # check if all chars up to position 8 are digits
+    first_9_digits = True
+    for char in s[:9]:
+        if char not in "0123456789":
+            first_9_digits = False
+            
+    # check if final char is a digit or X
+    last_digit_ok = True
+    if s[9] not in "0123456789X":
+        last_digit_ok = False
+    
+    # return True if all checks pass
+    return digits_10 and first_9_digits and last_digit_ok
 
 """
 ----------
