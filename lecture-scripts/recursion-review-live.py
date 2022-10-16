2ca0465482c3eff8fbaef033d2ec90b605f01413
Phil Chodrow <pchodrow@gmail.com>
2022-10-16 19:04:41 -0400

    add recursion review

---
+++ lecture-scripts/recursion-review-live.py
@@ -0,0 +1,88 @@
+"""
+-----------------
+REVIEW: Recursion
+-----------------
+
+In this lecture session, we'll work several review examples on the topic of recursion. 
+"""
+
+"""
+WARMUP: Write a recursive function absolute value function. 
+
+This function should, given a list L of integers, return a version of that list in which all negative numbers are replaced by their positive versions. 
+
+For example: 
+
+L = [-5, 4, 3, -2, 8]
+abs(L) 
+>>> [5, 4, 3, 2, 8] 
+"""
+
+def abs(L):
+    pass 
+    # base case: L is the empty list
+    
+    
+    # recursive step: return a list where: 
+    # 1. the 0th element has its sign adjusted if necessary
+    # 2. the abs() function is called on all of L *except* the first element. 
+    
+
+# test: uncomment when you're ready
+# L = [-5, 4, 3, -2, 8]
+# print(abs(L))
+
+"""
+It can be useful to write recursive functions with multiple arguments. Let's write a recursive implementation of the modulus operator %. Our function will be called mod(), and mod(x,y) will have the same value as x % y. For example, 
+
+mod(8, 3) 
+>>> 2
+"""
+
+def mod(x, y):
+    pass
+    # base case: what should the result be if x < y? 
+    
+    
+    # recursive step: 
+    #    use the mathematical relationship mod(x, y) = mod(x-y, y)
+    
+
+"""
+Many functions that we perform using for-loops can be instead performed recursively (note: this is not ALWAYS better, just different). Consider the function from a recent lecture on dictionaries. This function computes the count or frequency of each item in a string. 
+"""
+
+def character_counts(s):
+    d = {}
+    for char in s: 
+        if char not in d.keys():
+            d[char] = 1
+        else: 
+            d[char] = d[char] + 1  
+    return d
+    
+s = "Living simply makes loving simple." # -- bell hooks
+counts = character_counts(s)
+print(counts)
+
+"""
+We could instead write this function recursively! This requires passing two arguments to the function: both the string and a dictionary containing the counts. The dictionary starts off empty.  
+"""
+
+def recursive_count(s, counts = {}):
+    pass
+    # base case: 
+    #    if s is empty, return the counts
+
+    
+    # recursive step: 
+    #     process a single character of s and add it to counts
+    #     call the function recursively on the REST of s, with 
+    #        the updated counts
+
+# counts_2 = recursive_count(s)
+# print(counts == counts_2)
+
+"""
+This implementation is neither simpler nor faster, but it is recursive! 
+"""
---
+++ lecture-scripts/recursion-review.py
@@ -0,0 +1,84 @@
+"""
+-----------------
+REVIEW: Recursion
+-----------------
+
+In this lecture session, we'll work several review examples on the topic of recursion. 
+"""
+
+"""
+WARMUP: Write a recursive function absolute value function. 
+
+This function should, given a list L of integers, return a version of that list in which all negative numbers are replaced by their positive versions. 
+
+For example: 
+
+L = [-5, 4, 3, -2, 8]
+abs(L) 
+>>> [5, 4, 3, 2, 8] 
+"""
+
+def abs(L):
+    if L == []:
+        return L
+    else:
+        if L[0] < 0:
+            return [-L[0]] + abs(L[1:])
+        else:
+            return [L[0]] + abs(L[1:])
+
+L = [-5, 4, 3, -2, 8]
+print(abs(L))
+
+"""
+Let's write a recursive implementation of the modulus operator %. Our function will be called mod(), and mod(x,y) will have the same value as x % y. For example, 
+
+mod(8, 3) 
+>>> 2
+"""
+
+def mod(x, y):
+    if x < y:
+        return x
+    else:
+        return mod(x-y, y) 
+
+"""
+Many functions that we perform using for-loops can be instead performed recursively (note: this is not ALWAYS better, just different). Consider the function from a recent lecture on dictionaries. This function computes the count or frequency of each item in a string. 
+"""
+
+def character_counts(s):
+    d = {}
+    for char in s: 
+        if char not in d.keys():
+            d[char] = 1
+        else: 
+            d[char] = d[char] + 1  
+    return d
+    
+s = "Living simply makes loving simple." # -- bell hooks
+counts = character_counts(s)
+print(counts)
+
+"""
+We could instead write this function recursively! This requires passing two arguments to the function: both the string and a dictionary containing the counts. The dictionary starts off empty.  
+"""
+
+def recursive_count(s, counts = {}):
+
+    if len(s) == 0:
+        return counts 
+    else:
+        c = s[0]
+        if c not in counts.keys():
+            counts[c] = 1
+        else:
+            counts[c] = counts[c] + 1
+        return recursive_count(s[1:], counts)
+
+counts_2 = recursive_count(s)
+print(counts == counts_2)
+
+"""
+This implementation is neither simpler nor faster, but it is recursive! 
+"""
