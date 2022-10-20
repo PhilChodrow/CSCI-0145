ab8cf9679aa6eb8e51dc63ea2e3d8cf65907ad4a
Phil Chodrow <pchodrow@gmail.com>
2022-10-20 17:29:30 -0400

    Create images-1-live.py

---
+++ lecture-scripts/images-1-live.py
@@ -0,0 +1,30 @@
+# install the pillow and numpy modules
+# download middimage at
+# https://raw.githubusercontent.com/PhilChodrow/CSCI-0145/main/lecture-scripts/middimage.py
+# download the file at
+# https://raw.githubusercontent.com/PhilChodrow/CSCI-0145/main/assets/img/jorts.jpeg
+# save as jorts.jpeg
+
+
+
+# a simple image: 2x2 pixels
+
+
+# a simple color image: 1x2 pixels
+
+
+
+# activity: modify the pixel values to be:
+# your favorite color
+# your partner's favorite color
+
+# image from file: a beautiful orange boy
+
+
+# each pixel of im contains three values (RGB)
+
+
+# modifying an image: changing RGB values
+
+
+
