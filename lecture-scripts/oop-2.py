5a3a5dee98e653e3dd28a41f2795ff9c489e4e25
Phil Chodrow <pchodrow@gmail.com>
2022-11-01 21:46:31 -0400

    wrap text in oop lecture scripts

--- lecture-scripts/oop-2.py
+++ lecture-scripts/oop-2.py
@@ -3,13 +3,19 @@
 # -----------------------------------------
 
 """
-In this lecture, we'll work a slightly more complicated example with object-oriented programming. We'll see an example of the OOP workflow: start with a very simple, minimal class, add a small bit of functionality, test, document, and repeat until you've obtained the desired behavior. 
+In this lecture, we'll work a slightly more complicated example with
+object-oriented programming. We'll see an example of the OOP workflow: start
+with a very simple, minimal class, add a small bit of functionality, test,
+document, and repeat until you've obtained the desired behavior. 
 
-We're going to implement a Point class for representing points in the 2d plane. You can find a much more complete version of this example in the official lecture notes, including docstrings: 
+We're going to implement a Point class for representing points in the 2d plane.
+You can find a much more complete version of this example in the official
+lecture notes, including docstrings: 
 
 https://avaccari.gitlab.io/csci-0145/sp22/notes/object_oriented_programming.html
 
-Let's start with a minimal implementation. We'll initialize a Point with two arguments: an x-coordinate and a y-coordinate. 
+Let's start with a minimal implementation. We'll initialize a Point with two
+arguments: an x-coordinate and a y-coordinate. 
 """
 
 class Point:     
@@ -26,7 +32,8 @@ print(p.x)
 print(p.y)
 
 """
-This isn't very interesting though: classes that only *store data* are not very useful. Let's implement some more useful *behavior* for our class.   
+This isn't very interesting though: classes that only *store data* are not very
+useful. Let's implement some more useful *behavior* for our class.   
 """
 
 class Point:     
@@ -43,15 +50,24 @@ print(p.distance_to_origin())
 """
 That's more like it! 
 
-There's still more we can do to make the Point class useful. For example, printing p or typing it in the shell doesn't lead to a very good result. When I do either, for example, I get this: 
+There's still more we can do to make the Point class useful. For example,
+printing p or typing it in the shell doesn't lead to a very good result. When I
+do either, for example, I get this: 
 
 <__main__.Point at 0x7ff5800d3550>
 
-Not helpful! We can make something more helpful by implementing a special function called a *magic method*. A magic method is indicated by **two** underscores on either side. We've already seen an example of a magic method: __init__() is one! What makes magic methods "magic" is that they are called *implicitly*, without us explicitly calling them. In every class, instantiating it implicitly calls the __init__() method. So, when we do the line
+Not helpful! We can make something more helpful by implementing a special
+function called a *magic method*. A magic method is indicated by **two**
+underscores on either side. We've already seen an example of a magic method:
+__init__() is one! What makes magic methods "magic" is that they are called
+*implicitly*, without us explicitly calling them. In every class, instantiating
+it implicitly calls the __init__() method. So, when we do the line
 
 p = Point(3, 4)
 
-Python is *implicitly* calling Point.__init__(). There are lots of other magic methods. Let's look at another one: __repr__() controls what happens when we ask to inspect an object in the shell. 
+Python is *implicitly* calling Point.__init__(). There are lots of other magic
+methods. Let's look at another one: __repr__() controls what happens when we ask
+to inspect an object in the shell. 
 """
 
 class Point:     
@@ -79,9 +95,11 @@ print(p) # also affects what happens when we print
 INTERACTING OBJECTS
 -------------------
 
-A very important aspect of objects is that they can *interact with each other objects*, of the same class or different classes. 
+A very important aspect of objects is that they can *interact with each other
+objects*, of the same class or different classes. 
 
-For example, let's write a method that will allow one point to calculate its distance from another point: 
+For example, let's write a method that will allow one point to calculate its
+distance from another point: 
 """
 
 class Point:     
@@ -106,7 +124,8 @@ p1 = Point(-1, 2)
 p2 = Point(11, 15)
 
 """
-What are some quick tests you could perform to ensure that point.distance_to_point() is working correctly?
+What are some quick tests you could perform to ensure that
+point.distance_to_point() is working correctly?
 """
 
 first_test = p1.distance_to_point(p2) == p2.distance_to_point(p1)
@@ -120,13 +139,17 @@ print(second_test)
 DRAWING
 -------
 
-Now let's take our Point class and give it some *visualization* capabilities. Fortunately, we know just the tools for drawing: the Turtle module! 
+Now let's take our Point class and give it some *visualization* capabilities.
+Fortunately, we know just the tools for drawing: the Turtle module! 
 """
 
 import turtle
 
 """
-In order to make interesting points to draw, we are going to start by giving our points additional instance variables: a size and a color. This is a great example of the interplay between attributes and behavior: in order to support the complex behavior we want, we can add new attributes. 
+In order to make interesting points to draw, we are going to start by giving our
+points additional instance variables: a size and a color. This is a great
+example of the interplay between attributes and behavior: in order to support
+the complex behavior we want, we can add new attributes. 
 """
 
 class Point:     
