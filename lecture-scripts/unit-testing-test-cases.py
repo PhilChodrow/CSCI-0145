c4e0b77ee82300817fc79f0a39215591c97de5c3
Phil Chodrow <pchodrow@gmail.com>
2022-11-06 11:25:55 -0500

    Update unit-testing-test-cases.py

--- lecture-scripts/unit-testing-test-cases.py
+++ lecture-scripts/unit-testing-test-cases.py
@@ -1,12 +1,15 @@
 import unittest
 
-# this special syntax means that TestVector *inherits* all the functionality from unittest.TestCase. So, we get to use many methods that we haven't explicitly wrote -- we get them for free! 
+# this special syntax means that TestVector *inherits* all the functionality
+# from unittest.TestCase. So, we get to use many methods that we haven't
+# explicitly wrote -- we get them for free! 
 class TestVector(unittest.TestCase):
     
-    # all test methods need to begin with the word "test"
-    # test methods should take no additional arguments
+    # all test methods need to begin with the word "test" test methods should
+    # take no additional arguments
     def test_init(self):
         """
-        Check that a Vector can be initialized with user-specified x and y coordinates
+        Check that a Vector can be initialized with user-specified x and y
+        coordinates
         """
         v = Vector(2, 3)
